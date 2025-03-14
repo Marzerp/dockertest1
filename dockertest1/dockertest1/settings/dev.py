c79cf08c5e76ecaed93ff44e197f08ea07c48ab9
Marzerp <araceliromerozerpa@gmail.com>
2025-03-14 12:06:58 -0600

    buena praxis

---
+++ dockertest1/dockertest1/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t2=hgx#dcxo^rl)^xl_+^ff-43x=p#w80^yn$y&w013tt=@%(%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
